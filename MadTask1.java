8f251b2dc1543b415276ba75d660feede1d348d2
harshmodi15 <harshdmodi@gmail.com>
2021-06-17 20:34:08 +0530

    Dart Output 1

--- MadTask1.java
+++ MadTask1.java
@@ -3,9 +3,6 @@ import java.util.Scanner;
 class MadTask1 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int pScore = 0;
-        int cScore = 0;
-        int mScore = 0;
         int i = 1;
         Physics phy = new Physics();
         Chemistry chem = new Chemistry();
@@ -23,42 +20,42 @@ class MadTask1 {
                     phy.pQuestion1();
                     phy.pQuestion2();
                     phy.pQuestion3();
-                    System.out.println("Physics Score is " + pScore + " out of 3");
+                    System.out.println("Physics Score is " + phy.pScore + " out of 3");
                     break;
 
                 case ("a"):
                     phy.pQuestion1();
                     phy.pQuestion2();
                     phy.pQuestion3();
-                    System.out.println("Physics Score is " + pScore + " out of 3");
+                    System.out.println("Physics Score is " + phy.pScore + " out of 3");
                     break;
 
                 case ("B"):
                     chem.cQuestion1();
                     chem.cQuestion2();
                     chem.cQuestion3();
-                    System.out.println("Chemistry Score is " + cScore + " out of 3");
+                    System.out.println("Chemistry Score is " + chem.cScore + " out of 3");
                     break;
 
                 case ("b"):
                     chem.cQuestion1();
                     chem.cQuestion2();
                     chem.cQuestion3();
-                    System.out.println("Chemistry Score is " + cScore + " out of 3");
+                    System.out.println("Chemistry Score is " + chem.cScore + " out of 3");
                     break;
 
                 case ("C"):
                     ma.mQuestion1();
                     ma.mQuestion2();
                     ma.mQuestion3();
-                    System.out.println("Maths Score is " + mScore + " out of 3");
+                    System.out.println("Maths Score is " + ma.mScore + " out of 3");
                     break;
 
                 case ("c"):
                     ma.mQuestion1();
                     ma.mQuestion2();
                     ma.mQuestion3();
-                    System.out.println("Maths Score is " + mScore + " out of 3");
+                    System.out.println("Maths Score is " + ma.mScore + " out of 3");
                     break;
 
                 case ("D"):
@@ -80,110 +77,105 @@ class MadTask1 {
 
 class Physics {
     int pScore = 0;
+    Scanner sc = new Scanner(System.in);
     
     void pQuestion1() {           
-        Scanner sc = new Scanner(System.in);
         System.out.println("The force acting on a body for a short time are called as: ");
         System.out.println("A. Average Force");
         System.out.println("B. Momentum");
         System.out.println("C. Impulse");
         System.out.println("D. Tension");
         String p1 = sc.next();
-        if (p1 == "C" || p1 == "c") {
+        if (p1.equals("C") || p1.equals("c")) {
             System.out.println("You got the right answer");
             pScore = pScore + 1;
-        } else if (p1 == "A" || p1 == "B" || p1 == "D" || p1 == "a" || p1 == "b" || p1 == "d") {
+        } else if (p1.equals("A") || p1.equals("B") || p1.equals("D") || p1.equals("a") || p1.equals("b") || p1.equals("d")) {
             System.out.println("Sorry Wrong Answer. The Correct Option Is C");
         } else {
             System.out.println("Please give the Input in A, B, C or D");
-            //pQuestion1();
+            pQuestion1();
         }
-        sc.close();
+        
     }
 
     void pQuestion2() {
-        Scanner sc = new Scanner(System.in);
         System.out.println("The second law of motion gives us a measure of which of the following physical quantities?");
         System.out.println("A. Acceleration");
         System.out.println("B. Force");
         System.out.println("C. Power");
         System.out.println("D. Work Done");
         String p2 = sc.next();
-        if (p2 == "B" || p2 == "b") {
+        if (p2.equals("B") || p2.equals("b")) {
+            System.out.println("You got the right answer");
             pScore = pScore + 1;
-        } else if (p2 == "A" || p2 == "C" || p2 == "D" || p2 == "a" || p2 == "c" || p2 == "d") {
+        } else if (p2.equals("A") || p2.equals("C") || p2.equals("D") || p2.equals("a") || p2.equals("c") || p2.equals("d")) {
             System.out.println("Sorry Wrong Answer. The Correct Option Is B");
         } else {
             System.out.println("Please give the Input in A, B, C or D");
             pQuestion2();
         }
-        sc.close();
     }
 
     void pQuestion3() {
-        Scanner sc = new Scanner(System.in);
         System.out.println("The Universal Gravitational Constant for two bodies is dependent on which of the following?");
         System.out.println("A. Nature of the Bodies");
         System.out.println("B. Size of the Bodies");
         System.out.println("C. Nature of Medium Between the Bodies");
         System.out.println("D. None of the Above");
         String p3 = sc.next();
-        if (p3 == "D" || p3 == "d") {
+        if (p3.equals("D") || p3.equals("d")) {
+            System.out.println("You got the right answer");
             pScore = pScore + 1;
-        } else if (p3 == "A" || p3 == "B" || p3 == "C" || p3 == "a" || p3 == "b" || p3 == "c") {
+        } else if (p3.equals("A") || p3.equals("B") || p3.equals("C") || p3.equals("a") || p3.equals("b") || p3.equals("c")) {
             System.out.println("Sorry Wrong Answer. The Correct Option Is D");
         } else {
             System.out.println("Please give the Input in A, B, C or D");
             pQuestion3();
         }
-        sc.close();
     }
 }
 
 class Chemistry {
-
-    public static int cScore = 0;
+    Scanner sc = new Scanner(System.in);
+    int cScore = 0;
     
     public void cQuestion1() {
-        Scanner sc = new Scanner(System.in);
         System.out.println("The Modern periodic table has_____________:");
         System.out.println("A. Seven Periods and Eighteen Groups");
         System.out.println("B. Nine Periods and Eighten Groups");
         System.out.println("C. Seven Periods and Twenty One Groups");
         System.out.println("D. Nine Periods and Twenty One Groups");
         String c1 = sc.next();
-        if (c1 == "A" || c1 == "a") {
+        if (c1.equals("A") || c1.equals("a")) {
+            System.out.println("You got the right answer");
             cScore = cScore + 1;
-        } else if (c1 == "B" || c1 == "C" || c1 == "D" || c1 == "b" || c1 == "c" || c1 == "d") {
+        } else if (c1.equals("B") || c1.equals("C") || c1.equals("D") || c1.equals("b") || c1.equals("c") || c1.equals("d")) {
             System.out.println("Sorry Wrong Answer. The Correct Option Is A");
         } else {
             System.out.println("Please give the Input in A, B, C or D");
             cQuestion1();
         }
-        sc.close();
     }
 
     public void cQuestion2() {
-        Scanner sc = new Scanner(System.in);
         System.out.println("What is water gas?");
         System.out.println("A. Mixture of CO and H2");
         System.out.println("B. Mixture of CO and H2O");
         System.out.println("C. Mixture of CO2 and H2");
         System.out.println("D. Carbon Mono-oxide");
         String c2 = sc.next();
-        if (c2 == "A" || c2 == "a") {
+        if (c2.equals("A") || c2.equals("a")) {
+            System.out.println("You got the right answer");
             cScore = cScore + 1;
-        } else if (c2 == "B" || c2 == "C" || c2 == "D" || c2 == "b" || c2 == "c" || c2 == "d") {
+        } else if (c2.equals("B") || c2.equals("C") || c2.equals("D") || c2.equals("b") || c2.equals("c") || c2.equals("d")) {
             System.out.println("Sorry Wrong Answer. The Correct Option Is A");
         } else {
             System.out.println("Please give the Input in A, B, C or D");
             cQuestion2();
         }
-        sc.close();
     }
 
     public void cQuestion3() {
-        Scanner sc = new Scanner(System.in);
         System.out
                 .println("What will be the number of neutrons in an atom having atomic number 35 and mass number 80?");
         System.out.println("A. 35");
@@ -191,32 +183,33 @@ class Chemistry {
         System.out.println("C. 55");
         System.out.println("D. 80");
         String c3 = sc.next();
-        if (c3 == "B" || c3 == "b") {
+        if (c3.equals("B") || c3.equals("b")) {
+            System.out.println("You got the right answer");
             cScore = cScore + 1;
-        } else if (c3 == "A" || c3 == "C" || c3 == "D" || c3 == "a" || c3 == "c" || c3 == "d") {
+        } else if (c3.equals("A") || c3.equals("C") || c3.equals("D") || c3.equals("a") || c3.equals("c") || c3.equals("d")) {
             System.out.println("Sorry Wrong Answer. The Correct Option Is B");
         } else {
             System.out.println("Please give the Input in A, B, C or D");
             cQuestion3();
         }
-        sc.close();
     }
 }
 
 class Maths {
-    public static int mScore = 0;
+    int mScore = 0;
     Scanner sc = new Scanner(System.in);
 
     void mQuestion1() {
         System.out.println("What is the average of first 150 natural numbers?");
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        System.out.println("A. 70");
+        System.out.println("B. 70.5");
+        System.out.println("C. 75");
+        System.out.println("D. 75.5");
         String m1 = sc.next();
-        if (m1 == "a" || m1 == "A") {
+        if (m1.equals("D") || m1.equals("d")) {
+            System.out.println("You got the right answer");
             mScore = mScore + 1;
-        } else if (m1 == "A" || m1 == "B" || m1 == "C" || m1 == "a" || m1 == "b" || m1 == "c") {
+        } else if (m1.equals("A") || m1.equals("B") || m1.equals("C") || m1.equals("a") || m1.equals("b") || m1.equals("c")) {
             System.out.println("Sorry Wrong Answer. The Correct Option Is D");
         } else {
             System.out.println("Please give the Input in A, B, C or D");
@@ -231,9 +224,10 @@ class Maths {
         System.out.println("C. 18%");
         System.out.println("D. 20%");
         String m2 = sc.next();
-        if (m2 == "D" || m2 == "d") {
+        if (m2.equals("D") || m2.equals("d")) {
+            System.out.println("You got the right answer");
             mScore = mScore + 1;
-        } else if (m2 == "A" || m2 == "B" || m2 == "C" || m2 == "a" || m2 == "b" || m2 == "c") {
+        } else if (m2.equals("A") || m2.equals("B") || m2.equals("C") || m2.equals("a") || m2.equals("b") || m2.equals("c")) {
             System.out.println("Sorry Wrong Answer. The Correct Option Is D");
         } else {
             System.out.println("Please give the Input in A, B, C or D");
@@ -242,15 +236,16 @@ class Maths {
     }
 
     void mQuestion3() {
-        System.out.println("3) 19 + ……. = 42 \n A. 23 \n B. 61 \n C. 0 \n D. 42");
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        System.out.println("If A completes a particular work in 8 days and B completes the same work in 24 days. How many days will it take if they work together?");
+        System.out.println("A. 4");
+        System.out.println("B. 5");
+        System.out.println("C. 6");
+        System.out.println("D. 7");
         String m3 = sc.next();
-        if (m3 == "d" || m3 == "D") {
+        if (m3.equals("C") || m3.equals("c")) {
+            System.out.println("You got the right answer");
             mScore = mScore + 1;
-        } else if (m3 == "A" || m3 == "B" || m3 == "D" || m3 == "a" || m3 == "b" || m3 == "d") {
+        } else if (m3.equals("A") || m3.equals("B") || m3.equals("D") || m3.equals("a") || m3.equals("b") || m3.equals("d")) {
             System.out.println("Sorry Wrong Answer. The Correct Option Is C");
         } else {
             System.out.println("Please give the Input in A, B, C or D");
